6761bd750e7c73afc7de8f24838c3cbdc426a0ca
Vinnie <149159535+DasEinCrusader@users.noreply.github.com>
2026-07-22 14:31:51 +0100

    Initial commit - CO2 Biodiversity Tracker

---
+++ src/main/java/com/mycompany/co2trackerbiodiversity/CO2TrackerBiodiversity.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.co2trackerbiodiversity;
+
+/**
+ *
+ * @author 35385
+ */
+public class CO2TrackerBiodiversity {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
